e710790473174734d9227ce329e4a40bc09fd689
Luis22Ant <luisengenhariaeletronica@gmail.com>
2023-02-01 16:37:29 -0300

    Auto push

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,4 +3,4 @@ a=Convert.ToInt32(Console.ReadLine());
 b= Convert.ToInt32(Console.ReadLine());
 soma = a + b;
 Console.WriteLine("soma = " + soma);
-//Ele está enviando até com visual studio desligado
+
